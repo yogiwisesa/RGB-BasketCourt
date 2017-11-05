ff9f19fbd58fb5219f73206bdf022352ee36cae7
Yogi Wisesa <yogiwisesa@gmail.com>
2017-11-05 10:29:03 +0700

    Recyclerview finished

--- app/src/main/java/com/rubydev/basketcourt/HistoryAdapter.java
+++ app/src/main/java/com/rubydev/basketcourt/HistoryAdapter.java
@@ -1,32 +1,64 @@
 package com.rubydev.basketcourt;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Yogi Wisesa on 11/5/2017.
  */
 
 public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHolder>{
+    private List<Score> list = new ArrayList<>();
+    private Context context;
+
+    public HistoryAdapter(List<Score> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View itemView  = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item,parent, false);
+        return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        Score score = list.get(position);
+        holder.tvPertandingan.setText(score.getTeam_a() + " vs " + score.getTeam_b());
+        holder.tvTanggal.setText(score.getDate());
+        holder.tvTeamA.setText(score.getTeam_a());
+        holder.tvScoreA.setText("" + score.getScore_a());
+        holder.tvTeamB.setText(score.getTeam_b());
+        holder.tvScoreB.setText("" + score.getScore_b());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvPertandingan, tvTanggal, tvTeamA, tvScoreA,tvTeamB ,tvScoreB;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            tvPertandingan = itemView.findViewById(R.id.tvPertandingan);
+            tvTanggal = itemView.findViewById(R.id.tvTanggal);
+            tvTeamA = itemView.findViewById(R.id.tvTeamA);
+            tvScoreA = itemView.findViewById(R.id.tvScoreA);
+            tvTeamB = itemView.findViewById(R.id.tvTeamB);
+            tvScoreB = itemView.findViewById(R.id.tvScoreB);
+
         }
     }
 }
--- app/src/main/java/com/rubydev/basketcourt/MainActivity.java
+++ app/src/main/java/com/rubydev/basketcourt/MainActivity.java
@@ -3,15 +3,41 @@ package com.rubydev.basketcourt;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    List<Score> list = new ArrayList<>();
+    HistoryAdapter historyAdapter;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // inisiasi recycler dan adapter
+        recyclerView = findViewById(R.id.rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        historyAdapter = new HistoryAdapter(list, this);
+        recyclerView.setAdapter(historyAdapter);
+
+
+        // data palsu
+        list.add(new Score("A","B", 12,30, "30 Septermber 2017"));
+        list.add(new Score("A","B", 12,30, "30 Septermber 2017"));
+        list.add(new Score("A","B", 12,30, "30 Septermber 2017"));
+        list.add(new Score("A","B", 12,30, "30 Septermber 2017"));
+        list.add(new Score("A","B", 12,30, "30 Septermber 2017"));
+        list.add(new Score("A","B", 12,30, "30 Septermber 2017"));
+        list.add(new Score("A","B", 12,30, "30 Septermber 2017"));
+
+        // refresh data palsu
+        historyAdapter.notifyItemInserted(list.size());
     }
 
     @Override
